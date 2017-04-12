952a20e488774294d2883477c9eb7bbe5da0b3e3
jclu <jclu@users.noreply.github.com>
2017-04-12 16:47:59 +0800

    Create Module.py

---
+++ Module.py
@@ -0,0 +1,31 @@
+Modules
+
+There are three main types of modules in Python, 
+those you write yourself, 
+those you install from external sources, 
+and those that are preinstalled with Python. 
+
+The last type is called the standard library, and contains many useful modules. 
+Some of the standard library's useful modules include string, re, datetime, math, 
+random, os, multiprocessing, subprocess, socket, email, json, doctest, unittest, 
+pdb, argparse and sys.
+
+Tasks that can be done by the standard library include string parsing, data serialization, 
+testing, debugging and manipulating dates, emails, command line arguments, and much more!
+
+
+1) Install 3th party libraries
+Many third-party Python modules are stored on the Python Package Index (PyPI). 
+The best way to install these is using a program called pip. 
+This comes installed by default with modern distributions of Python. 
+If you don't have it, it is easy to install online. Once you have it, 
+installing libraries from PyPI is easy. 
+Look up the name of the library you want to install, 
+go to the command line (for Windows it will be the Command Prompt), 
+and enter pip install library_name. Once you've done this, 
+import the library and use it in your code.
+
+Using pip is the standard way of installing libraries on most operating systems, 
+but some libraries have prebuilt binaries for Windows. 
+These are normal executable files that let you install libraries with a GUI the same way you would install other programs.
+It's important to enter pip commands at the command line, not the Python interpreter.
