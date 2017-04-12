0554add5804ecf3440f38ca9336cf4e3bfb0c838
jclu <jclu@users.noreply.github.com>
2017-04-12 20:18:15 +0800

    Update Exception.py

--- Exception.py
+++ Exception.py
@@ -1,4 +1,4 @@
-Exceptions
+1) Exceptions
 
 Different exceptions are raised for different reasons. 
 Common exceptions:
@@ -11,3 +11,23 @@ ValueError: a function is called on a value of the correct type, but with an ina
 
 Python has several other built-in exceptions, such as ZeroDivisionError and OSError. 
 Third-party libraries also often define their own exceptions.
+
+
+2) Exception Handling
+A try statement can have multiple different except blocks to handle different exceptions.
+Multiple exceptions can also be put into a single except block using parentheses, to have the except block handle all of them.
+
+try:
+   variable = 10
+   print(variable + "hello")
+   print(variable / 2)
+except ZeroDivisionError:
+   print("Divided by zero")
+except (ValueError, TypeError):
+   print("Error occurred")
+
+try:
+   word = "spam"
+   print(word / 0)
+except:
+   print("An error occurred")
